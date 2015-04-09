8e3e986adfc335777040825ccfda680f1be88170
calit <thorsten@sealk.de>
2015-04-10 01:16:21 +0200

    Update mixxx.py
    arm7l is usual for arm (e. g. nvidia tegra k1 in my case)

--- build/mixxx.py
+++ build/mixxx.py
@@ -47,7 +47,7 @@ class MixxxBuild(object):
 
         if machine.lower() not in ['x86_64', 'x86', 'i686', 'i586',
                                    'alpha', 'hppa', 'mips', 'mipsel', 's390',
-                                   'sparc', 'ia64', 'armel', 'armhf', 'hurd-i386',
+                                   'sparc', 'ia64', 'armel', 'armhf', 'armv7l', 'hurd-i386',
                                    'sh3', 'sh4',
                                    'kfreebsd-amd64', 'kfreebsd-i386',
                                    'i486', 'i386', 'ppc', 'ppc64', 'powerpc',
